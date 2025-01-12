080bd482a5a8e09f5303b4bbe214a6a1627a0800
isabela-rodrigues <isabelarodriguescos@gmail.com>
2025-01-12 16:10:45 -0300

    inclusão da pasta hackerrank

---
+++ HackerRank/WarmUp/SimpleArraySum.py
@@ -0,0 +1,5 @@
+def simpleArraySum(ar):
+    soma = 0
+    for i in ar:
+        soma = soma + i
+    return soma
---
+++ HackerRank/WarmUp/SolveMeFirst.py
@@ -0,0 +1,9 @@
+def solveMeFirst(a,b):
+    soma = a + b
+    return soma
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
